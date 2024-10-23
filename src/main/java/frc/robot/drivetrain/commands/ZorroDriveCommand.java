d689455712eb20e8f65546aca11393dcda3968dc
Nate Laverdure <41549690+nlaverdure@users.noreply.github.com>
2024-10-23 10:54:04 -0400

    add getter methods to Zorro controller, prune robot constants (#7)

--- src/main/java/frc/robot/drivetrain/commands/ZorroDriveCommand.java
+++ src/main/java/frc/robot/drivetrain/commands/ZorroDriveCommand.java
@@ -4,37 +4,36 @@ package frc.robot.drivetrain.commands;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.Constants.OIConstants.Zorro;
+import frc.lib.ZorroController;
 import frc.robot.drivetrain.Drivetrain;
 
 public class ZorroDriveCommand extends DriveCommand {
 
-  Joystick m_controller;
+  ZorroController m_controller;
 
   public ZorroDriveCommand(
-      Drivetrain subsystem, SwerveDriveKinematics kinematicsType, Joystick joysticks) {
+      Drivetrain subsystem, SwerveDriveKinematics kinematicsType, ZorroController joystick) {
     super(subsystem, kinematicsType);
-    this.m_controller = joysticks;
+    this.m_controller = joystick;
   }
 
   @Override
   public double getX() {
-    return -MathUtil.applyDeadband(m_controller.getRawAxis(Zorro.kRightYAxis), 0.05);
+    return -MathUtil.applyDeadband(m_controller.getRightYAxis(), 0.05);
   }
 
   @Override
   public double getY() {
-    return -MathUtil.applyDeadband(m_controller.getRawAxis(Zorro.kRightXAxis), 0.05);
+    return -MathUtil.applyDeadband(m_controller.getRightXAxis(), 0.05);
   }
 
   @Override
   public double getTheta() {
-    return -MathUtil.applyDeadband(m_controller.getRawAxis(Zorro.kLeftXAxis), 0.05);
+    return -MathUtil.applyDeadband(m_controller.getLeftXAxis(), 0.05);
   }
 
   @Override
   public boolean fieldRelative() {
-    return m_controller.getRawButton(Zorro.kEUp);
+    return m_controller.getEUp();
   }
 }
