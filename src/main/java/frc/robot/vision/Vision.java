53fc150e696edebcc2af357f120f6106cef7f396
nlaverdure <nate.laverdure@gmail.com>
2025-01-11 18:05:44 -0500

    publish all cameras

--- src/main/java/frc/robot/vision/Vision.java
+++ src/main/java/frc/robot/vision/Vision.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import java.util.List;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import org.photonvision.EstimatedRobotPose;
 
@@ -17,8 +18,8 @@ public class Vision {
   public List<EstimatedPoseWithStdevs> getPoseEstimates() {
     record Tuple<T1,T2,T3> (T1 t1, T2 t2, T3 t3) {}
 
-    // return Arrays.stream(Camera.values())
-    return List.of(Camera.FrontRight).stream()
+    return Arrays.stream(Camera.values())
+    // return List.of(Camera.FrontRight).stream()
         .map(cam -> new Tuple<>(cam.getEstimatedGlobalPose(), cam.getEstimationStdDevs(), cam.toString()))
         .filter(tuple -> tuple.t1.isPresent())
         .map(tuple -> new EstimatedPoseWithStdevs(tuple.t1.get(), tuple.t2, tuple.t3))
