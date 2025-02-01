7a73491f82cb986f110de37afd45b0864b3f1f6c
nlaverdure <nate.laverdure@gmail.com>
2025-02-01 14:10:12 -0500

    add servo positions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -194,8 +194,8 @@ public final class Constants {
     public static final int kClimberCurrentLimit = 80;
 
     public static final int kRatchetServoPort = 0;
-    public static final double kEngagedPosition = 0.0;
-    public static final double kDisengedPosition = 1.0;
+    public static final double kEngagedPosition = 600.0/1024.0;
+    public static final double kDisengedPosition = 475.0/1024.0;
 
     public static final int kCageSensorPort = 1;
 
