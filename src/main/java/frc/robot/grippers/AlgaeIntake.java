8443b293b20894c09f713d028d7d505c0f812805
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2025-02-08 14:18:45 -0500

    added absolute encoders for the gripper wrists

--- src/main/java/frc/robot/grippers/AlgaeIntake.java
+++ src/main/java/frc/robot/grippers/AlgaeIntake.java
@@ -1,6 +1,7 @@
 package frc.robot.grippers;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -8,8 +9,12 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,101 +24,106 @@ import frc.robot.Constants.RobotConstants;
 
 public class AlgaeIntake extends SubsystemBase {
 
-  private final SparkMax intakeLeaderMotor =
+  private final SparkMax rollerLeaderMotor =
       new SparkMax(AlgaeIntakeConstants.kRollerLeaderMotorPort, MotorType.kBrushless);
-  private final SparkMax intakeFollowerMotor =
+  private final SparkMax rollerFollowerMotor =
       new SparkMax(AlgaeIntakeConstants.kRollerFollowerMotorPort, MotorType.kBrushless);
-  private final SparkMax rotationMotor =
+  private final SparkMax wristMotor =
       new SparkMax(AlgaeIntakeConstants.kWristMotorPort, MotorType.kBrushless);
 
-  private final SparkMaxConfig intakeConfig = new SparkMaxConfig();
-  private final SparkMaxConfig intakeFollowerConfig = new SparkMaxConfig();
-  private final SparkMaxConfig rotationConfig = new SparkMaxConfig();
+  private final SparkAbsoluteEncoder wristEncoder;
 
-  private final RelativeEncoder intakeEncoder;
-  private final RelativeEncoder rotationEncoder;
+  private final AbsoluteEncoderConfig absoluteEncoderConfig = new AbsoluteEncoderConfig();
 
-  private final SparkClosedLoopController intakeController;
-  private final SparkClosedLoopController rotationController;
+  private final SparkMaxConfig rollerConfig = new SparkMaxConfig();
+  private final SparkMaxConfig rollerFollowerConfig = new SparkMaxConfig();
+  private final SparkMaxConfig wristConfig = new SparkMaxConfig();
+
+  private final RelativeEncoder rollerEncoder;
+
+  private final SparkClosedLoopController rollerController;
+  private final SparkClosedLoopController wristController;
 
   private final DigitalInput coralSensor = new DigitalInput(AlgaeIntakeConstants.kAlgaeSensorPort);
 
   public AlgaeIntake() {
-    intakeConfig
+    wristEncoder = wristMotor.getAbsoluteEncoder();
+
+    absoluteEncoderConfig
+        .positionConversionFactor(AlgaeIntakeConstants
+        .kAlgaeWristPositionConversionFactor).inverted(false);
+
+    rollerConfig
         .voltageCompensation(RobotConstants.kNominalVoltage)
         .idleMode(IdleMode.kCoast)
         .smartCurrentLimit(RobotConstants.kDefaultNEO550CurretnLimit)
         .inverted(false);
 
-    rotationConfig
+    wristConfig
         .voltageCompensation(RobotConstants.kNominalVoltage)
         .idleMode(IdleMode.kBrake)
         .smartCurrentLimit(RobotConstants.kDefaultNEOCurrentLimit)
         .inverted(false);
 
-    intakeConfig
+    rollerConfig
         .closedLoop
         .p(AlgaeIntakeConstants.kVelocityP)
         .i(AlgaeIntakeConstants.kVelocityI)
         .d(AlgaeIntakeConstants.kVelocityD)
         .outputRange(-1, 1);
 
-    intakeConfig
+    rollerConfig
         .encoder
         .velocityConversionFactor(AlgaeIntakeConstants.kVelocityConversionFactor)
         .positionConversionFactor(AlgaeIntakeConstants.kPositionConversionFactor);
 
-    rotationConfig
+    wristConfig
         .closedLoop
         .p(AlgaeIntakeConstants.kPositionP)
         .i(AlgaeIntakeConstants.kPositionI)
         .d(AlgaeIntakeConstants.kPositionD)
-        .outputRange(-1, 1);
+        .outputRange(-1, 1)
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
 
-    rotationConfig
+    wristConfig
         .encoder
         .positionConversionFactor(AlgaeIntakeConstants.kAlgaeWristPositionConversionFactor);
 
-    intakeFollowerConfig.apply(intakeConfig).follow(intakeLeaderMotor);
+    rollerFollowerConfig.apply(rollerConfig).follow(rollerLeaderMotor);
 
-    intakeLeaderMotor.configure(
-        intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    rotationMotor.configure(
-        rotationConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    rollerLeaderMotor.configure(
+        rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    wristMotor.configure(
+        wristConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
-    intakeEncoder = intakeLeaderMotor.getEncoder();
-    rotationEncoder = rotationMotor.getEncoder();
+    rollerEncoder = rollerLeaderMotor.getEncoder();
 
-    intakeController = intakeLeaderMotor.getClosedLoopController();
-    rotationController = rotationMotor.getClosedLoopController();
+    rollerController = rollerLeaderMotor.getClosedLoopController();
+    wristController = wristMotor.getClosedLoopController();
   }
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Rotation Position", rotationEncoder.getPosition());
-    SmartDashboard.putNumber("Intake Velocity", intakeEncoder.getVelocity());
+    SmartDashboard.putNumber("Rotation Position", wristEncoder.getPosition());
+    SmartDashboard.putNumber("Intake Velocity", rollerEncoder.getVelocity());
 
     SmartDashboard.putBoolean("Coral Sensor", coralSensor.get());
   }
 
   private void resetIntakeEncoder() {
-    intakeEncoder.setPosition(0.0);
-  }
-
-  private void resetRotationEncoder() {
-    rotationEncoder.setPosition(0.0);
+    rollerEncoder.setPosition(0.0);
   }
 
   private void getRotationPosition() {
-    rotationEncoder.getPosition();
+    wristEncoder.getPosition();
   }
 
   private void setRotationPosition(double position) {
-    rotationController.setReference(position, ControlType.kPosition);
+    wristController.setReference(position, ControlType.kPosition);
   }
 
   private void setIntakeVelocity(double velocity) {
-    intakeController.setReference(velocity, ControlType.kVelocity);
+    rollerController.setReference(velocity, ControlType.kVelocity);
   }
 
   public Trigger hasCoral = new Trigger(() -> coralSensor.get());
--- src/main/java/frc/robot/grippers/CoralIntake.java
+++ src/main/java/frc/robot/grippers/CoralIntake.java
@@ -1,14 +1,18 @@
 package frc.robot.grippers;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,96 +23,97 @@ import frc.robot.Constants.RobotConstants;
 
 public class CoralIntake extends SubsystemBase {
 
-  private final SparkMax intakeMotor =
+  private final SparkMax rollerMotor =
       new SparkMax(CoralIntakeConstants.kRollerMotorPort, MotorType.kBrushless);
-  private final SparkMax rotationMotor =
+  private final SparkMax wristMotor =
       new SparkMax(CoralIntakeConstants.kWristMotorPort, MotorType.kBrushless);
 
-  private final SparkMaxConfig intakeConfig = new SparkMaxConfig();
-  private final SparkMaxConfig rotationConfig = new SparkMaxConfig();
+  private final SparkMaxConfig rollerConfig = new SparkMaxConfig();
+  private final SparkMaxConfig wristConfig = new SparkMaxConfig();
 
-  private final RelativeEncoder intakeEncoder;
-  private final RelativeEncoder rotationEncoder;
+  private final SparkAbsoluteEncoder wristEncoder;
 
-  private final SparkClosedLoopController intakeController;
-  private final SparkClosedLoopController rotationController;
+  private final AbsoluteEncoderConfig absoluteEncoderConfig = new AbsoluteEncoderConfig();
+
+  private final RelativeEncoder rollerEncoder;
+
+  private final SparkClosedLoopController rollerController;
+  private final SparkClosedLoopController wristController;
 
   private final DigitalInput coralSensor = new DigitalInput(CoralIntakeConstants.kCoralSensorPort);
 
   public CoralIntake() {
-    intakeConfig
+    wristEncoder = wristMotor.getAbsoluteEncoder();
+
+    rollerConfig
         .voltageCompensation(RobotConstants.kNominalVoltage)
         .idleMode(IdleMode.kCoast)
         .smartCurrentLimit(RobotConstants.kDefaultNEO550CurretnLimit)
         .inverted(false);
 
-    rotationConfig
+    wristConfig
         .voltageCompensation(RobotConstants.kNominalVoltage)
         .idleMode(IdleMode.kBrake)
         .smartCurrentLimit(RobotConstants.kDefaultNEO550CurretnLimit)
         .inverted(false);
 
-    intakeConfig
+    rollerConfig
         .closedLoop
         .p(CoralIntakeConstants.kVelocityP)
         .i(CoralIntakeConstants.kVelocityI)
         .d(CoralIntakeConstants.kVelocityD)
         .outputRange(-1, 1);
 
-    intakeConfig
+    rollerConfig
         .encoder
         .velocityConversionFactor(CoralIntakeConstants.kVelocityConversionFactor)
         .positionConversionFactor(CoralIntakeConstants.kPositionConversionFactor);
 
-    rotationConfig
+    wristConfig
         .closedLoop
         .p(CoralIntakeConstants.kPositionP)
         .i(CoralIntakeConstants.kPositionI)
         .d(CoralIntakeConstants.kPositionD)
-        .outputRange(-1, 1);
+        .outputRange(-1, 1)
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
 
-    rotationConfig
+    wristConfig
         .encoder
         .positionConversionFactor(CoralIntakeConstants.kCoralWristPositionConversionFactor);
 
-    intakeMotor.configure(
-        intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    rotationMotor.configure(
-        rotationConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    rollerMotor.configure(
+        rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    wristMotor.configure(
+        wristConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
-    intakeEncoder = intakeMotor.getEncoder();
-    rotationEncoder = rotationMotor.getEncoder();
+    rollerEncoder = rollerMotor.getEncoder();
 
-    intakeController = intakeMotor.getClosedLoopController();
-    rotationController = rotationMotor.getClosedLoopController();
+    rollerController = rollerMotor.getClosedLoopController();
+    wristController = wristMotor.getClosedLoopController();
   }
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Rotation Position", rotationEncoder.getPosition());
-    SmartDashboard.putNumber("Intake Velocity", intakeEncoder.getVelocity());
+    SmartDashboard.putNumber("Rotation Position", wristEncoder.getPosition());
+    SmartDashboard.putNumber("Intake Velocity", rollerEncoder.getVelocity());
 
     SmartDashboard.putBoolean("Coral Sensor", coralSensor.get());
   }
 
   private void resetIntakeEncoder() {
-    intakeEncoder.setPosition(0.0);
-  }
-
-  private void resetRotationEncoder() {
-    rotationEncoder.setPosition(0.0);
+    rollerEncoder.setPosition(0.0);
   }
 
   private void getRotationPosition() {
-    rotationEncoder.getPosition();
+    wristEncoder.getPosition();
   }
 
   private void setRotationPosition(double position) {
-    rotationController.setReference(position, ControlType.kPosition);
+    wristController.setReference(position, ControlType.kPosition);
   }
 
   private void setIntakeVelocity(double velocity) {
-    intakeController.setReference(velocity, ControlType.kVelocity);
+    rollerController.setReference(velocity, ControlType.kVelocity);
   }
 
   public Trigger hasCoral = new Trigger(() -> coralSensor.get());
