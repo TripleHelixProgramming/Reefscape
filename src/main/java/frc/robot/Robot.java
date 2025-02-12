a2e7e48a41516b13bfd0c2a6d688a4f9ca00f255
nlaverdure <nate.laverdure@gmail.com>
2025-02-11 19:12:26 -0500

    add event loop polling

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -84,6 +83,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
+    loop.poll();
     CommandScheduler.getInstance().run();
     checkVision();
     SmartDashboard.putData(driver.getHID());
