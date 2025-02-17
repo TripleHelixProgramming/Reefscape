c51ad72af9e7b2e8a14dbf052a0a246b04f203c3
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2025-02-17 17:48:14 -0500

    added binary switch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -98,7 +98,7 @@ public class Robot extends TimedRobot {
   public void disabledInit() {
     leds.setDefaultCommand(
         leds.createDisabledCommand(
-            autoSelector::getSwitchPosition,
+            autoSelector::getBinarySwitchPosition,
             allianceSelector::getAllianceColor,
             allianceSelector::agreementInAllianceInputs));
   }
