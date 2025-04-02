25062c50d9ec2b46170a4128432653b155b1a2c5
Christopher Larrieu <christopher@larrieu.org>
2025-04-02 10:27:51 -0400

    Clean up alternate 3piece

--- src/main/java/frc/robot/auto/BlueProcess3PieceAuto2.java
+++ src/main/java/frc/robot/auto/BlueProcess3PieceAuto2.java
@@ -3,10 +3,11 @@ package frc.robot.auto;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.drivetrain.Drivetrain;
 import frc.robot.elevator.CoralRoller;
 import frc.robot.elevator.Elevator;
-
 import java.util.Optional;
 
 public class BlueProcess3PieceAuto2 extends AutoMode {
@@ -39,98 +40,50 @@ public class BlueProcess3PieceAuto2 extends AutoMode {
     return blueCenterToL4F.getInitialPose();
   }
 
+  /**
+   * Attempts to score a coral at L4 height when the provided trajectory finishes.
+   *
+   * @param scoreTrajectory The active trajectory leading to the scoring position
+   * @param finalApprochTime The time in the trajectory when the elevator should raise
+   * @param nextAction an action to perform after the scoring is complete
+   */
+  protected void scoreToL4Then(
+      AutoTrajectory scoreTrajectory, double finalApprochTime, Command nextAction) {
+    scoreTrajectory.atTime(finalApprochTime).onTrue(elevator.coralL4PositionCG());
+
+    scoreTrajectory.doneDelayed(0.1).onTrue(coralRoller.intake().withTimeout(0.2));
+
+    scoreTrajectory.doneDelayed(0.4).onTrue(coralRoller.outtakeToL4().withTimeout(0.2));
+
+    scoreTrajectory.doneDelayed(0.6).onTrue(nextAction);
+  }
+
+  /**
+   * Grabs some coral once the provided trajectory finishes.
+   *
+   * @param sourceTrajectory The active trajectory leading to the coral source
+   * @param nextAction an action to perform after the coral is grabbed
+   */
+  protected void grabSomeCoralThen(AutoTrajectory sourceTrajectory, Command nextAction) {
+    sourceTrajectory
+        .done()
+        .onTrue(coralRoller.intake().until(coralRoller.hasCoral).andThen(nextAction));
+  }
+
   @Override
   public AutoRoutine getAutoRoutine() {
 
-    // spotless:off
-    blueProcess3PieceRoutine.active()
-        .onTrue(blueCenterToL4F.cmd());
+    // Score at L4F
+    blueProcess3PieceRoutine.active().onTrue(blueCenterToL4F.cmd());
+    scoreToL4Then(blueCenterToL4F, 3.0, blueL4FToSource.cmd());
 
-    /*
-     * 0 + 3 : At the dogleg, on final approach.
-     * */
-    blueCenterToL4F.atTime(3.0)
-        .onTrue(elevator.coralL4PositionCG());
+    // Grab some coral then score ate L4D
+    grabSomeCoralThen(blueL4FToSource, blueSourceToL4D.cmd());
+    scoreToL4Then(blueSourceToL4D, 3.2, blueL4DToSource.cmd());
 
-    /*
-     * L4F + 0.1 : Run coral intake briefly.
-     */
-    blueCenterToL4F.doneDelayed(0.1)
-        .onTrue(coralRoller.intake().withTimeout(0.2));
-
-    /*
-     * L4F + 0.4 : Outtake coral to L4.
-     */
-    blueCenterToL4F.doneDelayed(0.4)
-        .onTrue(coralRoller.outtakeToL4().withTimeout(0.2));
-
-    /*
-     * L4F + 0.6 : Head to source.
-     */
-    blueCenterToL4F.doneDelayed(0.6)
-        .onTrue(blueL4FToSource.cmd());
-
-    /*
-     * Source : Grab some coral and then head to L4D.
-     */
-    blueL4FToSource.done()
-        .onTrue(
-          coralRoller.intake()
-            .until(coralRoller.hasCoral)
-            .andThen(blueSourceToL4D.cmd()));
-
-    /*
-     * 0 + 3.2 : Raise elevator
-     */
-    blueSourceToL4D.atTime(3.2)
-        .onTrue(elevator.coralL4PositionCG());
-
-    /*
-     * L4D + 0.1 : Run coral intake briefly.
-     */
-    blueSourceToL4D.doneDelayed(0.1)
-        .onTrue(coralRoller.intake().withTimeout(0.2));
-
-    /*
-     * L4D + 0.4 : Outtake coral to L4.
-     */
-    blueSourceToL4D.doneDelayed(0.4)
-        .onTrue(coralRoller.outtakeToL4().withTimeout(0.2));
-
-    /*
-     * L4D + 0.6 : Head to source.
-     */
-    blueSourceToL4D.doneDelayed(0.6)
-        .onTrue(blueL4DToSource.cmd());
-
-    /*
-     * Source : Grab some coral and then head to L4C.
-     */
-    blueSourceToL4D.done()
-      .onTrue(
-        coralRoller.intake()
-          .until(coralRoller.hasCoral)
-          .andThen(blueSourceToL4C.cmd()));
-
-    /*
-     * 0 + 2.8 : Raise elevator
-     */
-    blueSourceToL4C.atTime(3.2)
-        .onTrue(elevator.coralL4PositionCG());
-
-    /*
-     * L4C + 0.1 : Run coral intake briefly.
-     */
-    blueSourceToL4C.doneDelayed(0.1)
-        .onTrue(coralRoller.intake().withTimeout(0.2));
-
-    /*
-     * L4C + 0.4 : Outtake coral to L4.
-     */
-    blueSourceToL4C.doneDelayed(0.4)
-        .onTrue(coralRoller.outtakeToL4().withTimeout(0.2));
-
-    // spotless:on
+    // Grab some coral then score at L4C
+    grabSomeCoralThen(blueL4DToSource, blueSourceToL4C.cmd());
+    scoreToL4Then(blueL4DToSource, 2.8, Commands.none());
 
     return blueProcess3PieceRoutine;
   }
